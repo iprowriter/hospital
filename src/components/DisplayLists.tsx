a3861e706014c8736db9db2ed205d2e5406861a4
martin <obinnamartin72@gmail.com>
2023-02-28 18:59:32 +0100

    data

--- src/components/DisplayLists.tsx
+++ src/components/DisplayLists.tsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import data from '../data/mock_data.json'
 
 function createData(
   name: string,
@@ -26,31 +27,33 @@ const rows = [
 ];
 
 export default function DisplayLists() {
+
+ const patients = data;
+ console.log(patients)
+
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 550 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell align="left">Firstname</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {patients.map((patient) => (
             <TableRow
-              key={row.name}
+              key={patient.patient_id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {patient.patient_id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="left">{patient.first_name}</TableCell>
+              <TableCell align="left">{patient.last_name}</TableCell>
+              <TableCell align="left">View Detail</TableCell>
             </TableRow>
           ))}
         </TableBody>
