015b87c387957c230cc5f320b19daab62cfc176b
martin <obinnamartin72@gmail.com>
2023-02-28 15:10:48 +0100

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import DisplayLists from "./components/DisplayLists";
 import Hero from "./components/Hero";
 import Header from "./components/navigation/Header";
+import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
 
 
 function App() {
@@ -7,6 +10,12 @@ function App() {
     <>
     <Header/>
     <Hero/>
+    <Container maxWidth="lg">
+        <Box sx={{ bgcolor: "#f5f5f5"}}>
+        <DisplayLists/>
+        </Box>
+      </Container>
+    
     </>
   );
 }
