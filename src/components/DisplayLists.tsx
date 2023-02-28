2da9c19cd2554a9f1049d7fbb6dee0c92fd07e99
moputa <moputa@vub.de>
2023-02-28 19:28:54 +0100

    table

--- src/components/DisplayLists.tsx
+++ src/components/DisplayLists.tsx
@@ -1,60 +1,70 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import data from '../data/mock_data.json'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import data from "../data/mock_data.json";
+import { Button } from "@mui/material";
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+
+
+
 
 export default function DisplayLists() {
-
- const patients = data;
- console.log(patients)
+  const patients = data;
+  console.log(patients);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 550 }} aria-label="simple table">
+    <TableContainer component={Paper} sx={{ marginTop: 20 }}>
+      <Table sx={{ minWidth: 500 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="left">Firstname</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell align="left">Firstname</StyledTableCell>
+            <StyledTableCell align="left">Lastname</StyledTableCell>
+            <StyledTableCell align="left">Detail</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {patients.map((patient) => (
-            <TableRow
-              key={patient.patient_id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
+            <StyledTableRow key={patient.patient_id}>
+              <StyledTableCell component="th" scope="row">
                 {patient.patient_id}
-              </TableCell>
-              <TableCell align="left">{patient.first_name}</TableCell>
-              <TableCell align="left">{patient.last_name}</TableCell>
-              <TableCell align="left">View Detail</TableCell>
-            </TableRow>
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                {patient.first_name}
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                {patient.last_name}
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                <Button>View Detail</Button>
+              </StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
